16ff196e982bd602932dd3f66964625a29a85913
Filip Lauc <filip.lauc93@gmail.com>
2017-01-30 13:49:04 +0100

    chore(package): Version bump :package:

--- src/confirmation.component.ts
+++ src/confirmation.component.ts
@@ -5,18 +5,22 @@ import {ResolveEmit} from './interfaces/resolve-emit';
     selector: 'jaspero-confirmation',
     styleUrls: ['./confirmation.css'],
     template: `
-        <div *ngIf="incomingData.overlay" class="overlay" (click)="overlayClick()"></div>
-        <div class="wrapper">
-            <div class="close" *ngIf="incomingData.showCloseButton" (click)="resolve({closedWithOutResolving: 'closeClick'})">
+        <div *ngIf="incomingData.overlay" class="jaspero__overlay" (click)="overlayClick()"></div>
+        <div class="jaspero__dialog">
+            <div class="jaspero__dialog-close" *ngIf="incomingData.showCloseButton" (click)="resolve({closedWithOutResolving: 'closeClick'})">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44">
                   <path d="M22 0C9.8 0 0 9.8 0 22s9.8 22 22 22 22-9.8 22-22S34.2 0 22 0zm3.2 22.4l7.5 7.5c.2.2.3.5.3.7s-.1.5-.3.7l-1.4 1.4c-.2.2-.5.3-.7.3-.3 0-.5-.1-.7-.3l-7.5-7.5c-.2-.2-.5-.2-.7 0l-7.5 7.5c-.2.2-.5.3-.7.3-.3 0-.5-.1-.7-.3l-1.4-1.4c-.2-.2-.3-.5-.3-.7s.1-.5.3-.7l7.5-7.5c.2-.2.2-.5 0-.7l-7.5-7.5c-.2-.2-.3-.5-.3-.7s.1-.5.3-.7l1.4-1.4c.2-.2.5-.3.7-.3s.5.1.7.3l7.5 7.5c.2.2.5.2.7 0l7.5-7.5c.2-.2.5-.3.7-.3.3 0 .5.1.7.3l1.4 1.4c.2.2.3.5.3.7s-.1.5-.3.7l-7.5 7.5c-.2.1-.2.5 0 .7z" fill="#FFF"/>
                 </svg>
             </div>
-            <div class="inner">
-                <p>{{incomingData.title}}</p>
-                <p>{{incomingData.message}}</p>
-                <button (click)="resolve({resolved: true})">{{incomingData.confirmText}}</button>
-                <button (click)="resolve({resolved: false})">{{incomingData.declineText}}</button>
+            <div class="jaspero__dialog-title">
+                {{incomingData.title}}
+            </div>
+            <div class="jaspero__dialog-content">
+                {{incomingData.message}}
+            </div>
+            <div class="jaspero__dialog-actions">
+                <button class="default" (click)="resolve({resolved: false})">{{incomingData.declineText}}</button>
+                <button class="primary" (click)="resolve({resolved: true})">{{incomingData.confirmText}}</button>
             </div>
         </div>
     `
@@ -27,6 +31,8 @@ export class ConfirmationComponent {
     ) {
         for (let key in this.incomingData) this.incomingData[key] = this._injector.get(key) || this.incomingData[key];
     }
+
+    animationState: string = 'enter';
 
     incomingData: any = {
         title: '',
