a97c2d4ff3a74db84d2c77d6ea0c8b891dccfaf8
Filip Lauc <filip.lauc93@gmail.com>
2017-05-12 12:11:20 +0200

    chore: Mayor package updates :hammer:

--- index.ts
+++
@@ -1,8 +0,0 @@
-export * from './src/confirmations.module';
-
-export * from './src/confirmations.component';
-export * from './src/confirmation.component';
-export * from './src/confirmations.service';
-export * from './src/interfaces/confirm-emit';
-export * from './src/interfaces/confirm-settings';
-export * from './src/interfaces/resolve-emit';
---
+++ src/lib/index.ts
@@ -0,0 +1,4 @@
+export {JasperoConfirmationsModule} from './src/module';
+export {ConfirmationsComponent} from './src/confirmations.component';
+export {ConfirmationComponent} from './src/confirmation.component';
+export {ConfirmationService} from './src/confirmations.service';
