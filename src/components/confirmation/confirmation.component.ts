e3f64bc6c96207b5c425505e4dae2d4e0aae8de5
Filip Lauc <filip.lauc93@gmail.com>
2017-11-17 13:48:46 +0100

    chore: Removed angular animations and switched to css for firefox support closes #6

--- src/components/confirmation/confirmation.component.ts
+++ src/components/confirmation/confirmation.component.ts
@@ -1,5 +1,4 @@
 import {Component, HostBinding, NgZone, ViewEncapsulation} from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 import {ResolveEmit} from '../../interfaces/resolve-emit';
 
 @Component({
@@ -7,22 +6,6 @@ import {ResolveEmit} from '../../interfaces/resolve-emit';
     templateUrl: './confirmation.component.html',
     styleUrls: ['./confirmation.component.scss'],
     encapsulation: ViewEncapsulation.None,
-    animations: [
-        trigger('overlayAn', [
-            state('void', style({opacity: 0})),
-            state('leave', style({opacity: 0})),
-            state('enter', style({opacity: 1})),
-            transition('void => enter', animate('400ms cubic-bezier(.25,.8,.25,1)')),
-            transition('enter => leave', animate('400ms cubic-bezier(.25,.8,.25,1)'))
-        ]),
-        trigger('wrapperAn', [
-            state('void', style({opacity: 0, transform: 'scale(0.75, 0.75) translate(0, 0)'})),
-            state('leave', style({opacity: 0, transform: 'scale(0.75, 0.75) translate(0, 0)'})),
-            state('enter', style({opacity: 1, transform: 'scale(1, 1) translate(0, 0)'})),
-            transition('void => enter', animate('450ms cubic-bezier(.5, 1.4, .5, 1)')),
-            transition('enter => leave', animate('450ms cubic-bezier(.5, 1.4, .5, 1)'))
-        ])
-    ]
 })
 export class ConfirmationComponent {
     constructor(
